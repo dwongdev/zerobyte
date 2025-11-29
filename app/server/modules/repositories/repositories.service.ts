1d4e7100ab2eb2d70dfe49abfdfe6dcbc5db2ea1
Nicolas Meienberger <github@thisprops.com>
2025-11-29 12:24:07 +0100

    fix: healtcheck, to not read full data

--- app/server/modules/repositories/repositories.service.ts
+++ app/server/modules/repositories/repositories.service.ts
@@ -249,7 +249,7 @@ const checkHealth = async (repositoryId: string) => {
 		throw new NotFoundError("Repository not found");
 	}
 
-	const { hasErrors, error } = await restic.check(repository.config, { readData: true });
+	const { hasErrors, error } = await restic.check(repository.config);
 
 	await db
 		.update(repositoriesTable)
